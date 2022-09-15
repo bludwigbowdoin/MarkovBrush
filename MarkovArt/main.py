d5808d9dfc4e52dc2561af215ee902601b9d6af8
bludwigbowdoin <bludwig@bowdoin.edu>
2022-09-15 18:17:52 -0400

    Removed the textures markov chain idea to produce a better aesthetic. Cleaned up the code with comments and description.

--- MarkovArt/main.py
+++ MarkovArt/main.py
@@ -1,4 +1,16 @@
-# Python code to simulate a moving paintbrush that relies on a Markov chain
+"""
+Bjorn Ludwig
+CSCI 3725
+M3: A Markov Distinction
+09/15/22
+The following Python code simulates a "paintbrush" that relies on a Markov chain to move, based on a random
+walk. One transition matrix determines the direction (north, east, south, west, pause, jump) based on the current
+direction, and the other transition matrix determines the color (blue, green, red, cyan, magenta, yellow) based
+off of the current color. The user may adjust any of the five variables starting in line 131, per the
+respective constraints. The user may also adjust the transition matrices starting in line 105, where the
+commented out matrix is an example of a variation.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,20 +20,18 @@ CIRCLE = "o"
 SQUARE = "s"
 DIAMOND = "D"
 
+
 class ArtWalker:
-    def __init__(self, direction_transition_matrix, color_transition_matrix, texture_transition_matrix):
+    def __init__(self, direction_transition_matrix, color_transition_matrix):
         """
         Args:
           direction_transition_matrix (dict): transition probabilities for the Markov chain
           color_transition_matrix (dict): color probabilities for the Markov chain
-          texture_transition_matrix (dict): texture probabilities for the Markov chain
         """
         self.direction_transition_matrix = direction_transition_matrix
         self.directions = list(direction_transition_matrix.keys())
         self.color_transition_matrix = color_transition_matrix
         self.colors = list(color_transition_matrix.keys())
-        self.texture_transition_matrix = texture_transition_matrix
-        self.textures = list(texture_transition_matrix.keys())
 
     def get_next_direction(self, current_direction):
         """ Determine the direction to move based on the current direction
@@ -44,45 +54,30 @@ class ArtWalker:
             p=[self.color_transition_matrix[current_color][next_color] for next_color in self.colors]
         )
 
-    def get_next_texture(self, current_texture):
-        """ Determine the next texture based on the current texture
-        Args:
-            current_texture (str): the current texture
-        """
-        return np.random.choice(
-            self.textures,
-            p=[self.texture_transition_matrix[current_texture][next_texture] for
-               next_texture in self.textures]
-        )
-
-    def walk_around(self, current_direction="N", current_color="blue", current_texture=10,
-                    path_length=100, jump_size=50):
+    def walk_around(self, current_direction="N", current_color="blue", path_length=100, jump_size=50):
         """Generating a path through a modified random walk with color and texture variation.
         Args:
             current_direction (str): starting direction
             current_color (str): starting color
-            current_texture (str): starting texture
             path_length (int): number of steps per path
             jump_size (int): size of a jump
         """
         x = np.zeros(path_length)
         y = np.zeros(path_length)
         colors = []
-        textures = []
 
-        # for each step in discrete time, determine an x-coord, y-coord, color, and texture
+        # for each step in discrete time, determine an x-coord, y-coord, and color
         for i in range(0, path_length):
             step_size = 1  # unless jump, the step_size is 1
             direction = self.get_next_direction(current_direction)
             current_color = self.get_next_color(current_color)
             colors.append(current_color)
-            current_texture = self.get_next_texture(current_texture)
-            textures.append(current_texture)
             current_direction = direction
 
             if direction == "J":  # in case of jump, modify size of jump for next direction
                 step_size = jump_size
                 direction = self.get_next_direction(current_direction)
+                current_direction = direction
             if direction == "N":  # move north
                 x[i] = x[i - 1]
                 y[i] = y[i - 1] + step_size
@@ -99,53 +94,53 @@ class ArtWalker:
                 x[i] = x[i - 1]
                 y[i] = y[i - 1]
 
-        this_path = np.vstack((x, y, colors, textures))
+        this_path = np.vstack((x, y, colors))
         return this_path
 
 
 def main():
-    walk_markov_a = ArtWalker({
-        "N": {"N": 0.02, "E": 0.9, "S": 0.0, "W": 0.02, "P": 0.02, "J": 0.04},
-        "E": {"N": 0.02, "E": 0.02, "S": 0.9, "W": 0.0, "P": 0.02, "J": 0.04},
-        "S": {"N": 0.0, "E": 0.02, "S": 0.02, "W": 0.9, "P": 0.02, "J": 0.04},
-        "W": {"N": 0.9, "E": 0.0, "S": 0.02, "W": 0.02, "P": 0.02, "J": 0.04},
-        "P": {"N": 0.2, "E": 0.2, "S": 0.2, "W": 0.2, "P": 0.2, "J": 0.0},
-        "J": {"N": 0.2, "E": 0.2, "S": 0.2, "W": 0.2, "P": 0.2, "J": 0.0},
-    },
+    walk_markov_a = ArtWalker(
         {
-            "blue":    {"blue": 0.5, "green": 0.15, "red": 0.05, "cyan": 0.2, "magenta": 0.05, "yellow": 0.05},
-            "green":   {"blue": 0.2, "green": 0.5, "red": 0.05, "cyan": 0.05, "magenta": 0.15, "yellow": 0.05},
-            "red":     {"blue": 0.05, "green": 0.15, "red": 0.5, "cyan": 0.05, "magenta": 0.2, "yellow": 0.05},
-            "cyan":    {"blue": 0.2, "green": 0.15, "red": 0.05, "cyan": 0.5, "magenta": 0.05, "yellow": 0.05},
-            "magenta": {"blue": 0.05, "green": 0.05, "red": 0.2, "cyan": 0.15, "magenta": 0.5, "yellow": 0.05},
-            "yellow":  {"blue": 0.05, "green": 0.2, "red": 0.15, "cyan": 0.05, "magenta": 0.05, "yellow": 0.5},
+            "N": {"N": 0.5, "E": 0.1, "S": 0.1, "W": 0.1, "P": 0.1, "J": 0.1},  # may adjust, but values of each
+            "E": {"N": 0.1, "E": 0.5, "S": 0.1, "W": 0.1, "P": 0.1, "J": 0.1},  # row MUST sum to 1.0
+            "S": {"N": 0.1, "E": 0.1, "S": 0.5, "W": 0.1, "P": 0.1, "J": 0.1},
+            "W": {"N": 0.1, "E": 0.1, "S": 0.1, "W": 0.5, "P": 0.1, "J": 0.1},
+            "P": {"N": 0.2, "E": 0.2, "S": 0.2, "W": 0.2, "P": 0.0, "J": 0.2},
+            "J": {"N": 0.2, "E": 0.2, "S": 0.2, "W": 0.2, "P": 0.2, "J": 0.0},
         },
+        # {
+        #     "N": {"N": 0.02, "E": 0.9, "S": 0.0, "W": 0.02, "P": 0.02, "J": 0.04},  # optional direction
+        #     "E": {"N": 0.02, "E": 0.02, "S": 0.9, "W": 0.0, "P": 0.02, "J": 0.04},  # matrix variation
+        #     "S": {"N": 0.0, "E": 0.02, "S": 0.02, "W": 0.9, "P": 0.02, "J": 0.04},
+        #     "W": {"N": 0.9, "E": 0.0, "S": 0.02, "W": 0.02, "P": 0.02, "J": 0.04},
+        #     "P": {"N": 0.2, "E": 0.2, "S": 0.2, "W": 0.2, "P": 0.0, "J": 0.2},
+        #     "J": {"N": 0.2, "E": 0.2, "S": 0.2, "W": 0.2, "P": 0.2, "J": 0.0},
+        # },
         {
-            POINT: {POINT: 0.6, PIXEL: 0.1, CIRCLE: 0.1, SQUARE: 0.1, DIAMOND: 0.1},
-            PIXEL: {POINT: 0.1, PIXEL: 0.6, CIRCLE: 0.1, SQUARE: 0.1, DIAMOND: 0.1},
-            CIRCLE: {POINT: 0.1, PIXEL: 0.1, CIRCLE: 0.6, SQUARE: 0.1, DIAMOND: 0.1},
-            SQUARE: {POINT: 0.1, PIXEL: 0.1, CIRCLE: 0.1, SQUARE: 0.6, DIAMOND: 0.1},
-            DIAMOND: {POINT: 0.1, PIXEL: 0.1, CIRCLE: 0.1, SQUARE: 0.1, DIAMOND: 0.6},
+            "blue": {"blue": 0.5, "green": 0.15, "red": 0.05, "cyan": 0.2, "magenta": 0.05, "yellow": 0.05},
+            "green": {"blue": 0.2, "green": 0.5, "red": 0.05, "cyan": 0.05, "magenta": 0.15, "yellow": 0.05},
+            "red": {"blue": 0.05, "green": 0.15, "red": 0.5, "cyan": 0.05, "magenta": 0.2, "yellow": 0.05},
+            "cyan": {"blue": 0.2, "green": 0.15, "red": 0.05, "cyan": 0.5, "magenta": 0.05, "yellow": 0.05},
+            "magenta": {"blue": 0.05, "green": 0.05, "red": 0.2, "cyan": 0.15, "magenta": 0.5, "yellow": 0.05},
+            "yellow": {"blue": 0.05, "green": 0.2, "red": 0.15, "cyan": 0.05, "magenta": 0.05, "yellow": 0.5},
         }
     )
 
+    # user may adjust the following 5 variables:
     start_direction = "S"  # "N" for north, "E" for east, "S" for south, "W" for west
     start_color = "cyan"  # "blue", "green", "red", "cyan", "magenta", or "yellow"
-    current_texture = CIRCLE  # 0.5, 0.75, 1.0, 1.25, or 1.5
+    texture = CIRCLE  # POINT, PIXEL, CIRCLE, SQUARE, or DIAMOND
     path_length = 1000  # integer from 1 to 10,000 (above 10,000 can take a while)
-    jump_size = 10  # integer 1 or greater
+    jump_size = 1  # integer 1 or greater
 
-    path_a = walk_markov_a.walk_around(start_direction, start_color, current_texture, path_length, jump_size)
+    path_a = walk_markov_a.walk_around(start_direction, start_color, path_length, jump_size)
     x_a = path_a[0][:]
     y_a = path_a[1][:]
     colors_a = path_a[2][:]
-    textures_a = path_a[3][:]
 
-    # plotting stuff:
-    plt.title("MarkovBrush (" + str(path_length) + " steps)")
+    plt.title("MarkovBrush (" + str(path_length) + " steps, jump size of " + str(jump_size) + ")")
     for i in range(len(path_a[0][:])):  # plotting the corresponding x and y with respective color and texture
-        plt.scatter(x_a[i], y_a[i], s=20, c=colors_a[i], marker=textures_a[i], linewidth=0)
-        # plt.scatter(x_a[i], y_a[i], s=20, c=colors_a[i], marker=',', linewidth=0)
+        plt.scatter(x_a[i], y_a[i], s=10, c=colors_a[i], marker=texture, linewidth=0)
 
     plt.axis('off')
     plt.gca().set_aspect('equal', adjustable='box')  # maintain 1:1 scale of the plot axes
